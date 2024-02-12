a20950c67cc9dd0a531c6a21623683b2106dcd41
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2024-02-12 22:38:12 +0700

    upadte user service

--- server/service/user.go
+++ server/service/user.go
@@ -5,11 +5,24 @@ import "server/model"
 type CreateAccount = model.CreateAccount
 type UserResponse model.UserResponse
 type ResponseUserHistory = model.UserHistory
+type OrderRequest = model.OrderRequest
+type BalanceHistory = model.BalanceHistory
+type UserStock = model.UserStock
 
 type UserService interface {
 	CreateUserAccount(CreateAccount) (string, error)
+	DepositBalance(string, float64) (string, error)
+	WithdrawBalance(string, float64) (string, error)
+	BuyStock(OrderRequest) (string, error)
+	SaleStock(OrderRequest) (string, error)
+	SetFavoriteStock(string, string) (string, error)
+	GetUserBalanceHistory(string, string, uint) ([]BalanceHistory, error)
+	GetUserBalance(string) (float64, error)
+	GetFavoriteStock(string) ([]string, error)
 	GetUserAccount(string) (UserResponse, error)
 	GetUserTradingHistories(string) ([]ResponseUserHistory, error)
 	GetUserStockHistory(string) ([]ResponseUserHistory, error)
+	GetUserStockAmount(string, string) (UserStock, error) 
+	DeleteFavoriteStock(string, string) (string, error)
 	DeleteUserAccount(string, string) (string, error)
 }
--- server/service/user_service.go
+++ server/service/user_service.go
@@ -16,6 +16,38 @@ func (s userService) CreateUserAccount(CreateAccount) (string, error) {
 	return "", nil
 }
 
+func (s userService) DepositBalance(string, float64) (string, error) {
+	return "", nil
+}
+
+func (s userService) WithdrawBalance(string, float64) (string, error) {
+	return "", nil
+}
+
+func (s userService) BuyStock(OrderRequest) (string, error) {
+	return "", nil
+}
+
+func (s userService) SaleStock(OrderRequest) (string, error) {
+	return "", nil
+}
+
+func (s userService) SetFavoriteStock(string, string) (string, error) {
+	return "", nil
+}
+
+func (s userService) GetUserBalanceHistory(string, string, uint) ([]BalanceHistory, error) {
+	return []BalanceHistory{}, nil
+}
+
+func (s userService) GetUserBalance(string) (float64, error) {
+	return 0, nil
+}
+
+func (s userService) GetFavoriteStock(string) ([]string, error) {
+	return []string{}, nil
+}
+
 func (s userService) GetUserAccount(string) (UserResponse, error) {
 	return UserResponse{}, nil
 }
@@ -26,6 +58,14 @@ func (s userService) GetUserTradingHistories(string) ([]ResponseUserHistory, err
 
 func (s userService) GetUserStockHistory(string) ([]ResponseUserHistory, error) {
 	return []ResponseUserHistory{}, nil
+}
+
+func (s userService) GetUserStockAmount(string, string) (UserStock, error) {
+	return UserStock{}, nil
+}
+
+func (s userService) DeleteFavoriteStock(string, string) (string, error) {
+	return "", nil
 }
 
 func (s userService) DeleteUserAccount(string, string) (string, error) {
